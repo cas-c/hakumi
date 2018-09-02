4f8d4f545238dca3be0b739bef8d03cc684cee23
lipgloss <gloss.water@gmail.com>
2018-09-02 17:03:37 -0600

    move embeds

---
+++ src/embeds.ts
@@ -0,0 +1,65 @@
+import Discord from "discord.js";
+import { DateTime } from "luxon";
+
+const simpleEmbed = (user: Discord.User, title: string) => {
+    return new Discord.MessageEmbed()
+        .setTitle(title)
+        .addField(user.username, `${user} \n ${user.id}`)
+        .setThumbnail(user.displayAvatarURL())
+        .setTimestamp(new Date());
+};
+
+const richerEmbed = (member: Discord.GuildMember, title: string) => {
+    return new Discord.MessageEmbed()
+        .setTitle(title)
+        .setDescription(
+            `${member} ${member.displayName} ${member.user.username}#${
+                member.user.discriminator
+            }`,
+        )
+        .addField(
+            "Created",
+            DateTime.fromMillis(member.user.createdTimestamp).toFormat(
+                "yyyy LLL dd HH:mm",
+            ),
+            true,
+        )
+        .addField(
+            "Joined",
+            DateTime.fromMillis(member.joinedTimestamp).toFormat(
+                "yyyy LLL dd HH:mm",
+            ),
+            true,
+        )
+        .addField("Roles", member.roles.array().join(" "))
+        .setColor(config.color)
+        .setThumbnail(member.user.displayAvatarURL())
+        .setTimestamp(new Date());
+};
+//  .setDescription(message.url);
+
+const deleteEmbed = (message: Discord.Message) => {
+    const response = new Discord.MessageEmbed()
+        .setTitle(
+            `Message from ${message.author.username} deleted in #${
+                (message.channel as Discord.TextChannel).name
+            }`,
+        )
+        .addField("Links", `${message.author} @ ${message.channel}`)
+        .setTimestamp(new Date())
+        .setColor(config.color);
+    // Things that may or may not exist.
+    if (message.cleanContent) {
+        response.addField("Deleted Text", message.cleanContent);
+    }
+    const attachment = message.attachments.first();
+    if (attachment && attachment.proxyURL) {
+        response.addField(
+            "Deleted Attachment",
+            `[View Attachment](${attachment.proxyURL})`,
+        );
+    }
+    return response;
+};
+
+export { richerEmbed, deleteEmbed, simpleEmbed };
--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,9 @@
-import Discord, { TextChannel } from "discord.js";
-import { DateTime } from "luxon";
+import Discord from "discord.js";
 import config from "./config";
+import { deleteEmbed, richerEmbed, simpleEmbed } from "./embeds";
 
 const Hakumi = new Discord.Client();
 let home: Discord.TextChannel;
-
-const simpleEmbed = (user: Discord.User, title: string) => {
-    return new Discord.MessageEmbed()
-        .setTitle(title)
-        .addField(user.username, `${user} \n ${user.id}`)
-        .setThumbnail(user.displayAvatarURL())
-        .setTimestamp(new Date());
-};
-
-const richerEmbed = (member: Discord.GuildMember, title: string) => {
-    return new Discord.MessageEmbed()
-        .setTitle(title)
-        .setDescription(
-            `${member} ${member.displayName} ${member.user.username}#${
-                member.user.discriminator
-            }`,
-        )
-        .addField(
-            "Created",
-            DateTime.fromMillis(member.user.createdTimestamp).toFormat(
-                "yyyy LLL dd HH:mm",
-            ),
-            true,
-        )
-        .addField(
-            "Joined",
-            DateTime.fromMillis(member.joinedTimestamp).toFormat(
-                "yyyy LLL dd HH:mm",
-            ),
-            true,
-        )
-        .addField("Roles", member.roles.array().join(" "))
-        .setColor(config.color)
-        .setThumbnail(member.user.displayAvatarURL())
-        .setTimestamp(new Date());
-};
-//  .setDescription(message.url);
-
-const deleteEmbed = (message: Discord.Message) => {
-    const response = new Discord.MessageEmbed()
-        .setTitle(
-            `Message from ${message.author.username} deleted in #${
-                (message.channel as TextChannel).name
-            }`,
-        )
-        .addField("Links", `${message.author} @ ${message.channel}`)
-        .setTimestamp(new Date())
-        .setColor(config.color);
-    // Things that may or may not exist.
-    if (message.cleanContent) {
-        response.addField("Deleted Text", message.cleanContent);
-    }
-    const attachment = message.attachments.first();
-    if (attachment && attachment.proxyURL) {
-        response.addField(
-            "Deleted Attachment",
-            `[View Attachment](${attachment.proxyURL})`,
-        );
-    }
-    return response;
-};
 
 Hakumi.on("ready", () => {
     console.log(`logged in as ${Hakumi.user.tag}.`);
