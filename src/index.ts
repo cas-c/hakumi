512ddddb08331e082e7513beb677a0dab8600abb
lipgloss <gloss.water@gmail.com>
2018-09-02 19:43:11 -0600

    add custom reaction listener for dispensing roles

--- src/index.ts
+++ src/index.ts
@@ -61,15 +61,45 @@ Hakumi.on("guildBanRemove", (guild: Discord.Guild, user: Discord.User) => {
     home.send(simpleEmbed(user, "Unban"));
 });
 
+const reactionEvents: any = {
+    MESSAGE_REACTION_ADD: "customReactionAdd",
+};
+
+Hakumi.on("raw", async (event: any) => {
+    if (!config.reactionListener) return;
+    if (!reactionEvents.hasOwnProperty(event.t)) return;
+    const { d: data } = event;
+    if (data.message_id !== config.reactionMessage) return;
+    const member = Hakumi.guilds
+        .get(config.home.guild)
+        .members.get(data.user_id);
+    if (!member) return;
+
+    const channel = Hakumi.channels.get(data.channel_id);
+
+    if ((channel as Discord.TextChannel).messages.has(data.message_id)) return;
+
+    const message = await (channel as Discord.TextChannel).messages.fetch(
+        data.message_id,
+    );
+    const emojiKey = data.emoji.id || data.emoji.name;
+    const reaction =
+        message.reactions.get(emojiKey) || message.reactions.add(data);
+
+    Hakumi.emit(reactionEvents[event.t], reaction, member);
+    if (message.reactions.size === 1) message.reactions.delete(emojiKey);
+});
+
+Hakumi.on(
+    "customReactionAdd",
+    (reaction: Discord.ReactionEmoji, member: Discord.GuildMember) => {
+        member.roles.add([member.guild.roles.get(config.reactionRole)]);
+    },
+);
+
 Hakumi.on("disconnect", () => home.send(`disconnecting... <@${config.owner}>`));
 Hakumi.on("reconnecting", () => home.send("reconnected."));
-
-Hakumi.on("error", error => {
-    home.send(`<@${config.owner}> \n` + error.message);
-});
-
-Hakumi.on("warn", warning => {
-    home.send(`<@${config.owner}> \n` + warning);
-});
+Hakumi.on("error", error => home.send(`<@${config.owner}> \n` + error.message));
+Hakumi.on("warn", warning => home.send(`<@${config.owner}> \n` + warning));
 
 Hakumi.login(config.token);
