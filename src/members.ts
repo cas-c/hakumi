15da72ee4048409d3b2f8d6572c335c5bbe5a708
lipgloss <gloss.water@gmail.com>
2018-10-06 00:09:43 -0600

    not flexible enough apparantly

--- src/members.ts
+++ src/members.ts
@@ -4,12 +4,9 @@ import config from "./config";
 const joinClub = (message: Discord.Message, exemptFromDelete: boolean) => {
     const member = message.member;
     if (!member) return;
-    if (
-        message.cleanContent.toLowerCase() !== config.club.code &&
-        !exemptFromDelete &&
-        message.deletable
-    )
-        message.delete();
+    if (message.cleanContent.toLowerCase() !== config.club.code) {
+        if (!exemptFromDelete && message.deletable) message.delete();
+    }
     member.roles.add([member.guild.roles.get(config.club.role)]);
 };
 
